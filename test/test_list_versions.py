32dd7ce4e8a22752ecc8d780cd67d108118685a0
Bob Gray <bobgray1000@gmail.com>
2021-02-10 19:52:28 -0500

    Docstrings and formatting

--- test/test_list_versions.py
+++ test/test_list_versions.py
@@ -1,44 +1,75 @@
-import unittest
 import os
-from pydock.pydock import ComposeFileManager
+import unittest
 from unittest.mock import Mock
-config = {"general":{}}
+
+from pydock.pydock import ComposeFileManager
+
+config = {"general": {}}
 config["general"]["versions"] = 5
 config["general"]["compose_file"] = "/docker/docker-compose.yml"
 
+
 class ListVersionsTest(unittest.TestCase):
-    
     def test_only_compose_file(self):
+        """ Test case where only compose file is in directory. """
+
         os.listdir = Mock()
         test_cfm = ComposeFileManager(config)
         os.listdir.return_value = ["docker-compose.yml"]
         file_list = test_cfm.list_versions()
         expected_list = ["docker-compose.yml"]
         self.assertEqual(file_list, expected_list)
-    
+
     def test_multiple_backups_found(self):
+        """ Test case where compose file plus multiple backups are in directory. """
+
         os.listdir = Mock()
         test_cfm = ComposeFileManager(config)
-        os.listdir.return_value = ["docker-compose.yml","docker-compose.yml.3","docker-compose.yml.1","docker-compose.yml.2"]
+        os.listdir.return_value = [
+            "docker-compose.yml",
+            "docker-compose.yml.3",
+            "docker-compose.yml.1",
+            "docker-compose.yml.2",
+        ]
         file_list = test_cfm.list_versions()
-        expected_list = ["docker-compose.yml","docker-compose.yml.1","docker-compose.yml.2","docker-compose.yml.3"]
+        expected_list = [
+            "docker-compose.yml",
+            "docker-compose.yml.1",
+            "docker-compose.yml.2",
+            "docker-compose.yml.3",
+        ]
         self.assertEqual(file_list, expected_list)
-    
+
     def test_other_files_found(self):
+        """ Test case where other non-related files are in directory. """
+
         os.listdir = Mock()
         test_cfm = ComposeFileManager(config)
-        os.listdir.return_value = ["docker-compose.yml","whatever.txt","docker-compose.save","docker-compose.yml.1"]
+        os.listdir.return_value = [
+            "docker-compose.yml",
+            "whatever.txt",
+            "docker-compose.save",
+            "docker-compose.yml.1",
+        ]
         file_list = test_cfm.list_versions()
-        expected_list = ["docker-compose.yml","docker-compose.yml.1"]
+        expected_list = ["docker-compose.yml", "docker-compose.yml.1"]
         self.assertEqual(file_list, expected_list)
-    
+
     def test_non_conforming_files_found(self):
+        """ Test case where docker-compose.yml.XXX (not an integer) files are found in directory """
+
         os.listdir = Mock()
         test_cfm = ComposeFileManager(config)
-        os.listdir.return_value = ["docker-compose.yml","whatever.txt","docker-compose.yml.save","docker-compose.yml.1"]
+        os.listdir.return_value = [
+            "docker-compose.yml",
+            "whatever.txt",
+            "docker-compose.yml.save",
+            "docker-compose.yml.1",
+        ]
         file_list = test_cfm.list_versions()
-        expected_list = ["docker-compose.yml","docker-compose.yml.1"]
+        expected_list = ["docker-compose.yml", "docker-compose.yml.1"]
         self.assertEqual(file_list, expected_list)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
--- test/test_rotate_files.py
+++ test/test_rotate_files.py
@@ -1,63 +1,90 @@
-import unittest
+import logging
 import os
-from pydock.pydock import ComposeFileManager
+import unittest
 from unittest.mock import Mock
 from unittest.mock import call
-import logging
-config = {"general":{}}
+
+from pydock.pydock import ComposeFileManager
+
+
+config = {"general": {}}
 config["general"]["versions"] = 3
 config["general"]["compose_file"] = "/docker/docker-compose.yml"
 
-#
-# Create a ComposeFileManager
-# use Mock to test what happens when rotate_files is called
 
-# logging.basicConfig(level=logging.DEBUG)
 class RotateFilesTest(unittest.TestCase):
-    
     def test_only_compose_file(self):
+        """ Test file rotation with only compose file. """
+
         test_cfm = ComposeFileManager(config)
         test_cfm.list_versions = Mock()
         os.remove = Mock()
         os.rename = Mock()
         test_cfm.list_versions.return_value = ["docker-compose.yml"]
         test_cfm.rotate_files()
-        os.remove.assert_not_called() 
-        os.rename.assert_not_called() 
-    
-    def test_compose_with_one_backup(self):
-        test_cfm = ComposeFileManager(config)
-        test_cfm.list_versions = Mock()
-        os.remove = Mock()
-        os.rename = Mock()
-        test_cfm.list_versions.return_value = ["docker-compose.yml", "docker-compose.yml.1"]
-        test_cfm.rotate_files()
         os.remove.assert_not_called()
-        os.rename.assert_called_once_with("docker-compose.yml.1", "docker-compose.yml.2")
+        os.rename.assert_not_called()
 
-    
-    def test_skip_a_backup(self):
+    def test_compose_with_one_backup(self):
+        """ Test file rotation with compose file and a backup. """
+
         test_cfm = ComposeFileManager(config)
         test_cfm.list_versions = Mock()
         os.remove = Mock()
         os.rename = Mock()
-        test_cfm.list_versions.return_value = ["docker-compose.yml", "docker-compose.yml.1", "docker-compose.yml.3"]
+        test_cfm.list_versions.return_value = [
+            "docker-compose.yml",
+            "docker-compose.yml.1",
+        ]
         test_cfm.rotate_files()
         os.remove.assert_not_called()
-        calls = [call("docker-compose.yml.3", "docker-compose.yml.4"), call("docker-compose.yml.1", "docker-compose.yml.2")]
+        os.rename.assert_called_once_with(
+            "docker-compose.yml.1", "docker-compose.yml.2"
+        )
+
+    def test_skip_a_backup(self):
+        """ Test file rotation if a backup is skipped. """
+
+        test_cfm = ComposeFileManager(config)
+        test_cfm.list_versions = Mock()
+        os.remove = Mock()
+        os.rename = Mock()
+        test_cfm.list_versions.return_value = [
+            "docker-compose.yml",
+            "docker-compose.yml.1",
+            "docker-compose.yml.3",
+        ]
+        test_cfm.rotate_files()
+        os.remove.assert_not_called()
+        calls = [
+            call("docker-compose.yml.3", "docker-compose.yml.4"),
+            call("docker-compose.yml.1", "docker-compose.yml.2"),
+        ]
         os.rename.assert_has_calls(calls, any_order=False)
 
-    
     def test_too_many_backups(self):
+        """Test file rotation if we start with more than the
+        number of configured backups."""
+
         test_cfm = ComposeFileManager(config)
         test_cfm.list_versions = Mock()
         os.remove = Mock()
         os.rename = Mock()
-        test_cfm.list_versions.return_value = ["docker-compose.yml", "docker-compose.yml.1", "docker-compose.yml.2", "docker-compose.yml.3", "docker-compose.yml.4"]
+        test_cfm.list_versions.return_value = [
+            "docker-compose.yml",
+            "docker-compose.yml.1",
+            "docker-compose.yml.2",
+            "docker-compose.yml.3",
+            "docker-compose.yml.4",
+        ]
         test_cfm.rotate_files()
         os.remove.assert_called_once_with("docker-compose.yml.4")
-        calls = [call("docker-compose.yml.2", "docker-compose.yml.3"),call("docker-compose.yml.1", "docker-compose.yml.2")]
+        calls = [
+            call("docker-compose.yml.2", "docker-compose.yml.3"),
+            call("docker-compose.yml.1", "docker-compose.yml.2"),
+        ]
         os.rename.assert_has_calls(calls, any_order=False)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
